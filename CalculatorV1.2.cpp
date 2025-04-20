b5cb2dcc8bdcbdab4befc68e1751046d273484fc
laplesz <vatamaniuk.danylo@chnu.edu.ua>
2025-04-20 11:51:36 +0300

    Update CalculatorV1.2.cpp
    A few problems solved (now you can enter int and double in one row)

--- CalculatorV1.2.cpp
+++ CalculatorV1.2.cpp
@@ -3,56 +3,83 @@
 
 using namespace std;
 
-int action;
-double result, num1, num2;
+int choose_operation, int_result, int_num1, int_num2;
+double double_result, double_num1, double_num2, check_num1, check_num2;
 
-namespace math_operators
+namespace math_operators // не міняти, рване
 {
-	int sum(int num1, int num2);
-	double sum(double num1, double num2);
-	int substruct(int num1, int num2);
-	double substruct(double num1, double num2);
-	int multiply(int num1, int num2);
-	double multiply(double num1, double num2);
-	int divide(int num1, int num2);
-	double divide(double num1, double num2);
-	int power(int result, int num1, int num2);
-	double power(double result, double num1, double num2);
-	int module(int result, int num1);
-	double module(double result, double num1);
-	int root(int num1);
-	double root(double num1);
+	int sum(int int_num1, int int_num2);
+	double sum(double double_num1, double double_num2);
+	double sum(int int_num1, double double_num2);
+	double sum(double double_num1, int int_num2);
+
+	int substruct(int int_num1, int int_num2);
+	double substruct(double double_num1, double double_num2);
+	double substruct(int int_num1, double double_num2);
+	double substruct(double double_num1, int int_num2);
+
+	int multiply(int int_num1, int int_num2);
+	double multiply(double double_num1, double double_num2);
+	double multiply(int int_num1, double double_num2);
+	double multiply(double double_num1, int int_num2);
+
+	int divide(int int_num1, int int_num2);
+	double divide(double double_num1, double double_num2);
+	double divide(int int_num1, double double_num2);
+	double divide(double double_num1, int int_num2);
+
+	int power(int int_num1, int int_num2);
+	double power(double double_num1, double double_num2);
+
+	int module(int int_result, int int_num1);
+	double module(double double_result, double double_num1);
+
+	double root(int int_num1); // пішов проти системи
+	double root(double double_num1);
 }
 
-int math_operators::sum(int num1, int num2) { return num1 + num2; }
-double math_operators::sum(double num1, double num2) { return num1 + num2; }
-int math_operators::substruct(int num1, int num2) { return num1 - num2; }
-double math_operators::substruct(double num1, double num2) { return num1 - num2; }
-int math_operators::multiply(int num1, int num2) { return num1 * num2; }
-double math_operators::multiply(double num1, double num2) { return num1 * num2; }
-int math_operators::divide(int num1, int num2) { return num1 / num2; }
-double math_operators::divide(double num1, double num2) { return num1 / num2; }
-int math_operators::power(int result, int num1, int num2) {
-	result = 1;
-	for (int i = 0; i < num2; i++)
-	{
-		result *= num1;
-	}
-	return result;
-}
-double math_operators::power(double result, double num1, double num2) {
-	result = 1;
-	for (int i = 0; i < num2; i++)
-	{
-		result *= num1;
-	}
-	return result;
-}
-int math_operators::module(int result, int num1) { return result < 0 ? -num1 : num1; }
-double math_operators::module(double result, double num1) { return result < 0 ? -num1 : num1; }
-int math_operators::root(int num1) { return sqrt(num1); }
-double math_operators::root(double num1) { return sqrt(num1); }
+int math_operators::sum(int int_num1, int int_num2) { return int_num1 + int_num2; } // не міняти, рване
+double math_operators::sum(double double_num1, double double_num2) { return double_num1 + double_num2; }
+double math_operators::sum(int int_num1, double double_num2) { return int_num1 + double_num2; }
+double math_operators::sum(double double_num1, int int_num2) { return double_num1 + int_num2; }
 
+int math_operators::substruct(int int_num1, int int_num2) { return int_num1 - int_num2; }
+double math_operators::substruct(double double_num1, double double_num2) { return double_num1 - double_num2; }
+double math_operators::substruct(int int_num1, double double_num2) { return int_num1 - double_num2; }
+double math_operators::substruct(double double_num1, int int_num2) { return double_num1 - int_num2; }
+
+int math_operators::multiply(int int_num1, int int_num2) { return int_num1 * int_num2; }
+double math_operators::multiply(double double_num1, double double_num2) { return double_num1 * double_num2; }
+double math_operators::multiply(int int_num1, double double_num2) { return int_num1 * double_num2; }
+double math_operators::multiply(double double_num1, int int_num2) { return double_num1 * int_num2; }
+
+int math_operators::divide(int int_num1, int int_num2) { return int_num1 / int_num2; }
+double math_operators::divide(double double_num1, double double_num2) { return double_num1 / double_num2; }
+double math_operators::divide(int int_num1, double double_num2) { return int_num1 / double_num2; }
+double math_operators::divide(double double_num1, int int_num2) { return double_num1 / int_num2; }
+
+int math_operators::power(int int_num1, int int_num2) {
+	int int_result = 1;
+	for (int i = 0; i < int_num2; i++)
+	{
+		int_result *= int_num1;
+	}
+	return int_result;
+}
+double math_operators::power(double double_num1, double double_num2) {
+	double double_result = 1;
+	for (int i = 0; i < double_num2; i++)
+	{
+		double_result *= double_num1;
+	}
+	return double_result;
+}
+
+int math_operators::module(int int_result, int int_num1) { return int_result < 0 ? -1 * int_num1 : int_num1; }
+double math_operators::module(double double_result, double double_num1) { return double_result < 0 ? -1 * double_num1 : double_num1; }
+
+double math_operators::root(int int_num1) { return sqrt(int_num1); }
+double math_operators::root(double double_num1) { return sqrt(double_num1); }
 
 void calculator()
 {
@@ -61,68 +88,210 @@ void calculator()
 	cout << "2: -" << endl;
 	cout << "3: *" << endl;
 	cout << "4: /" << endl;
-	cout << "5: ^x" << endl;
+	cout << "5: ^y" << endl;
 	cout << "6: |x|" << endl;
 	cout << "7: x^1/2" << endl;
 	cout << "Enter a number to choose operation: ";
-	cin >> action;
+	cin >> choose_operation;
 
-	switch (action)
+	switch (choose_operation)
 	{
 	case 1:
 	{
 		cout << "x + y" << endl;
 		cout << "Input 2 numbers: ";
-		cin >> num1 >> num2;
-		cout << num1 << " + " << num2 << " = " << math_operators::sum(num1, num2);
+		cin >> check_num1 >> check_num2;
+		int_num1 = check_num1;
+		int_num2 = check_num2;
+		if (check_num1 == int_num1 && check_num2 == int_num2)
+		{
+			cout << int_num1 << " + " << int_num2 << " = " << math_operators::sum(int_num1, int_num2);
+		}
+		else if (check_num1 == int_num1 && check_num2 != int_num2)
+		{
+			double_num2 = check_num2;
+			cout << int_num1 << " + " << double_num2 << " = " << math_operators::sum(int_num1, double_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 == int_num2)
+		{
+			double_num1 = check_num1;
+			cout << double_num1 << " + " << int_num2 << " = " << math_operators::sum(double_num1, int_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 != int_num2)
+		{
+			double_num1 = check_num1;
+			double_num2 = check_num2;
+			cout << double_num1 << " + " << double_num2 << " = " << math_operators::sum(double_num1, double_num2);
+		}
+		else
+		{
+			cout << "For now this version doesn't have this option";
+		}
 		break;
 	}
 	case 2:
 	{
 		cout << "x - y" << endl;
 		cout << "Input 2 numbers: ";
-		cin >> num1 >> num2;
-		cout << num1 << " - " << num2 << " = " << math_operators::substruct(num1, num2);
+		cin >> check_num1 >> check_num2;
+		int_num1 = check_num1;
+		int_num2 = check_num2;
+		if (check_num1 == int_num1 && check_num2 == int_num2)
+		{
+			cout << int_num1 << " - " << int_num2 << " = " << math_operators::substruct(int_num1, int_num2);
+		}
+		else if (check_num1 == int_num1 && check_num2 != int_num2)
+		{
+			double_num2 = check_num2;
+			cout << int_num1 << " - " << double_num2 << " = " << math_operators::substruct(int_num1, double_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 == int_num2)
+		{
+			double_num1 = check_num1;
+			cout << double_num1 << " - " << int_num2 << " = " << math_operators::substruct(double_num1, int_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 != int_num2)
+		{
+			double_num1 = check_num1;
+			double_num2 = check_num2;
+			cout << double_num1 << " - " << double_num2 << " = " << math_operators::substruct(double_num1, double_num2);
+		}
+		else
+		{
+			cout << "For now this version doesn't have this option";
+		}
 		break;
 	}
 	case 3:
 	{
 		cout << "x * y" << endl;
 		cout << "Input 2 numbers: ";
-		cin >> num1 >> num2;
-		cout << num1 << " * " << num2 << " = " << math_operators::multiply(num1, num2);
+		cin >> check_num1 >> check_num2;
+		int_num1 = check_num1;
+		int_num2 = check_num2;
+		if (check_num1 == int_num1 && check_num2 == int_num2)
+		{
+			cout << int_num1 << " * " << int_num2 << " = " << math_operators::multiply(int_num1, int_num2);
+		}
+		else if (check_num1 == int_num1 && check_num2 != int_num2)
+		{
+			double_num2 = check_num2;
+			cout << int_num1 << " * " << double_num2 << " = " << math_operators::multiply(int_num1, double_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 == int_num2)
+		{
+			double_num1 = check_num1;
+			cout << double_num1 << " * " << int_num2 << " = " << math_operators::multiply(double_num1, int_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 != int_num2)
+		{
+			double_num1 = check_num1;
+			double_num2 = check_num2;
+			cout << double_num1 << " * " << double_num2 << " = " << math_operators::multiply(double_num1, double_num2);
+		}
+		else
+		{
+			cout << "For now this version doesn't have this option";
+		}
 		break;
 	}
 	case 4:
 	{
 		cout << "x / y" << endl;
 		cout << "Input 2 numbers: ";
-		cin >> num1 >> num2;
-		cout << num1 << " / " << num2 << " = " << math_operators::divide(num1, num2);
+		cin >> check_num1 >> check_num2;
+		int_num1 = check_num1;
+		int_num2 = check_num2;
+		if (check_num1 == int_num1 && check_num2 == int_num2)
+		{
+			cout << int_num1 << " / " << int_num2 << " = " << math_operators::divide(int_num1, int_num2);
+		}
+		else if (check_num1 == int_num1 && check_num2 != int_num2)
+		{
+			double_num2 = check_num2;
+			cout << int_num1 << " / " << double_num2 << " = " << math_operators::divide(int_num1, double_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 == int_num2)
+		{
+			double_num1 = check_num1;
+			cout << double_num1 << " / " << int_num2 << " = " << math_operators::divide(double_num1, int_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 != int_num2)
+		{
+			double_num1 = check_num1;
+			double_num2 = check_num2;
+			cout << double_num1 << " / " << double_num2 << " = " << math_operators::divide(double_num1, double_num2);
+		}
+		else
+		{
+			cout << "For now this version doesn't have this option";
+		}
 		break;
 	}
-	case 5:
-	{
-		cout << "^x" << endl;
+	case 5: // з цим все ще якась лажа; спосіб Стьопи може і працює, але не було враховано головного фактору - я не настільки розумний
+	{       // я вирішив не робити більше умов як у попередніх суто через цю помилку
+		cout << "x^y" << endl;
+		cout << "This version cannot calculate operations with 'double' values correctly. Use it at your own risk." << endl;
 		cout << "Input 2 numbers: ";
-		cin >> num1 >> num2;
-		cout << num1 << "^" << num2 << " = " << math_operators::power(result, num1, num2);
+		cin >> check_num1 >> check_num2;
+		int_num1 = check_num1;
+		int_num2 = check_num2;
+		if (check_num1 == int_num1 && check_num2 == int_num2)
+		{
+			cout << int_num1 << "^" << int_num2 << " = " << math_operators::power(int_num1, int_num2);
+		}
+		else if (check_num1 != int_num1 && check_num2 != int_num2)
+		{
+			double_num1 = check_num1;
+			double_num2 = check_num2;
+			cout << double_num1 << "^" << double_num2 << " = " << math_operators::power(double_num1, double_num2);
+		}
+		else
+		{
+			cout << "For now this version doesn't have this option";
+		}
 		break;
 	}
 	case 6:
 	{
 		cout << "|x|" << endl;
 		cout << "Input 1 number: ";
-		cin >> num1;
-		cout << "|" << num1 << "| = " << math_operators::module(result, num1);
+		cin >> check_num1;
+		int_num1 = check_num1;
+		if (check_num1 == int_num1)
+		{
+			cout << "| " << int_num1 << " | = " << math_operators::module(int_result, int_num1);
+		}
+		else if (check_num1 != int_num1)
+		{
+			double_num1 = check_num1;
+			cout << "|" << double_num1 << "| = " << math_operators::module(double_result, double_num1);
+		}
+		else
+		{
+			cout << "For now this version doesn't have this option";
+		}
 		break;
 	}
 	case 7:
 	{
 		cout << "x^1/2" << endl;
 		cout << "Input 1 number: ";
-		cin >> num1;
-		cout << num1 << "^1/2 = " << math_operators::root(result);
+		cin >> check_num1;
+		int_num1 = check_num1;
+		if (check_num1 == int_num1)
+		{
+			cout << int_num1 << "^1/2 = " << math_operators::root(int_num1);
+		}
+		else if (check_num1 != int_num1)
+		{
+			double_num1 = check_num1;
+			cout << double_num1 << "^1/2 = " << math_operators::root(double_num1);
+		}
+		else
+		{
+			cout << "For now this version doesn't have this option";
+		}
 		break;
 	}
 	default:
@@ -137,7 +306,3 @@ int main()
 	calculator();
 	return 0;
 }
-
-
-
-
